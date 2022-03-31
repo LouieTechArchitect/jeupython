543d20cb589c493407210ce3f33c664d3695d97b
Louie <77439151+LouieTechArchitect@users.noreply.github.com>
2022-03-31 14:41:39 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+echo "# jeupython" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/LouieTechArchitect/jeupython.git
+git push -u origin main
